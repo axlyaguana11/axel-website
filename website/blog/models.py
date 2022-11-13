df2d2656c86c4431612d55056c1ab2bbe23d7298
axlyaguana11 <axl9809@gmail.com>
2022-11-13 14:42:29 -0500

    Post model created

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,5 +1,24 @@
 from django.db import models
+from django.utils import timezone
+import datetime 
+
+PUBLISHED = (
+    (False, 'Draft'),
+    (True, 'Published')
+)
 
 # Create your models here.
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    meta_description = models.CharField(max_length=255, blank=True)
+    date_created = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(choices=PUBLISHED, default=False)
+
+    class Meta:
+        ordering = ['-date_created']
+    
+    def __str__(self):
+        return self.title
