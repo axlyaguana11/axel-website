e69db3f78e62b522b4ecad10eb07148a7a9aba15
axlyaguana11 <axl9809@gmail.com>
2022-11-14 11:11:48 -0500

    Profile and Tag models created

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 import datetime 
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+    bio = models.CharField(max_length=240, blank=True)
+
+    def __str__(self):
+        return self.user.get_username()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name 
 
 PUBLISHED = (
     (False, 'Draft'),
@@ -16,6 +34,9 @@ class Post(models.Model):
     date_created = models.DateTimeField(auto_now=True)
     publish_date = models.DateTimeField(blank=True, null=True)
     published = models.BooleanField(choices=PUBLISHED, default=False)
+
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     class Meta:
         ordering = ['-date_created']
